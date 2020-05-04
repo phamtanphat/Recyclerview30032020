e8f7d121b7d5459d29ca8aaa7c54df92c0e15bec
phatdroid94 <phatdroid94@gmail.com>
2020-05-04 20:46:05 +0700

    setup adapter

---
+++ app/src/main/java/com/example/recyclerview30032020/FoodAdapter.java
@@ -0,0 +1,58 @@
+package com.example.recyclerview30032020;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodHolder> {
+    private List<Food> mFoodList;
+
+    public FoodAdapter(List<Food> mFoodList) {
+        this.mFoodList = mFoodList;
+    }
+
+    @NonNull
+    @Override
+    public FoodHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Truyen layout vao de khoi tao ra FoodHolder
+        // Can truyen item_food
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.item_food, parent, false);
+        return new FoodHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FoodHolder holder, int position) {
+
+    }
+
+    // Hien thi so luong dong trong danh sach
+    @Override
+    public int getItemCount() {
+        return mFoodList != null ? mFoodList.size() : 0;
+    }
+
+    class FoodHolder extends RecyclerView.ViewHolder{
+        ImageView img;
+        TextView tvName,tvPrice,tvAddress;
+        public FoodHolder(@NonNull View itemView) {
+            super(itemView);
+            img = itemView.findViewById(R.id.imageFood);
+            tvName = itemView.findViewById(R.id.textviewName);
+            tvPrice = itemView.findViewById(R.id.textviewPrice);
+            tvAddress = itemView.findViewById(R.id.textviewAddress);
+        }
+    }
+
+}
--- app/src/main/java/com/example/recyclerview30032020/MainActivity.java
+++ app/src/main/java/com/example/recyclerview30032020/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.recyclerview30032020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,7 @@ public class MainActivity extends AppCompatActivity {
         // 2 : Tạo 1 dòng item cho recyclerview
         // 3 : khởi tạo adapter gắn dữ liệu cho từng dòng
         // 4 : setup và map adapter vào cho recyclerview
+
     }
+
 }
