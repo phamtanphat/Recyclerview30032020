d02e92decc866a17b685c8cbb125d5c248b4a01f
phatdroid94 <phatdroid94@gmail.com>
2020-05-06 21:01:03 +0700

    add item finish

--- app/src/main/java/com/example/recyclerview30032020/MainActivity.java
+++ app/src/main/java/com/example/recyclerview30032020/MainActivity.java
@@ -1,16 +1,13 @@
 package com.example.recyclerview30032020;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -51,13 +48,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addNewFood() {
-        mFoods.add(
-                new Food(
-                        "Mon an so " + count++,
-                        "Quan " + new Random().nextInt(10) + 1,
-                        new Random().nextInt(19001) + 10000,
-                        R.drawable.hinh_mon_banh_khot
-                ));
+
+//        mFoodAdapter.notifyDataSetChanged();
+//        Log.d("BBB",mFoods.size() + "");
+        ArrayList<Food> foods = new ArrayList<>();
+        Food food = new Food(
+                "Mon an so " + count++,
+                "Quan " + new Random().nextInt(10) + 1,
+                new Random().nextInt(19001) + 10000,
+                R.drawable.hinh_mon_banh_khot
+        );
+        foods.add(food);
+        // Khi lam viec voi server gan du lieu tu server ve roi cap nhat
+        mFoods = foods;
+        mFoodAdapter.notifyDataSetChanged();
+        Log.d("BBB",mFoods.size() + "");
+
     }
 
 }
